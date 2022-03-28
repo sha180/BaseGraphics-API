d02fca06b6c607b0eed14c2a5c9a689d15ab2615
sha180 <sha18039@byui.edu>
2022-03-28 12:17:31 -0600

    health attribute

---
+++ LocalLib/Casting/atributes/AttributeHealth.cs
@@ -0,0 +1,35 @@
+using LocalLib.Types;
+using System.Collections.Generic;
+
+
+namespace LocalLib.Casting
+{
+    /// <summary>
+    /// A thing that participates in the game.
+    /// </summary>
+    public class AttributeHealth : Attribute
+    {
+        
+        private string attributeKey = AttributeKey.TrackAble;
+        
+        public int health;
+
+        // private Point position;
+        // private Point size;
+        // private Point velocity;
+
+        /// <summary>
+        /// Constructs a new instance of Actor.
+        /// </summary>
+        public AttributeHealth(int health)
+        {
+            this.health = health;
+        }
+
+        public string GetAttributeKey()
+        {
+            return attributeKey;
+        }
+
+    }
+}
