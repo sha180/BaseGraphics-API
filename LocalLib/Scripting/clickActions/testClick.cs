1e18dd7865f620e9af9c799b3f5cde890880db4a
sha180 <sha18039@byui.edu>
2022-03-28 13:35:27 -0600

    testbutton

--- LocalLib/Scripting/clickActions/testClick.cs
+++ LocalLib/Scripting/clickActions/testClick.cs
@@ -1,15 +1,26 @@
-namespace LocalLib.Scripting
-{
-    public class testClick : ActionCallback
-    {
-        public testClick() 
-        {
+using LocalLib.Casting;
+using LocalLib.Services;
 
+namespace LocalLib.Scripting.Actions
+{
+    /// <summary>
+    /// A thing that is done in the game.
+    /// </summary>
+    public class TestClick : Action
+    {
+        public TestClick()
+        {
         }
 
-        public void OnNext(string sceneName)
+        /// <summary>
+        /// Executes something that is important in the game. This method should be overriden by 
+        /// derived classes.
+        /// </summary>
+        /// <param name="cast">The cast of actors.</param>
+        /// <param name="script">The script of actions.</param>
+        public void Execute(Cast forground = null, Cast midground = null, Cast background = null, Script script = null, ActionCallback callback = null)
         {
-
+            // videoService.ClearBuffer(true);
         }
     }
 }
