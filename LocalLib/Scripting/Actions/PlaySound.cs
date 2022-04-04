32c5dede2b894edb2189375d6b6f0326556d96ce
BlazerB3n <pit20003@byui.edu>
2022-04-04 12:07:39 -0600

    sound comment

--- LocalLib/Scripting/Actions/PlaySound.cs
+++ LocalLib/Scripting/Actions/PlaySound.cs
@@ -1,26 +1,26 @@
-using LocalLib.Casting;
-using LocalLib.Types;
-using LocalLib.Services;
+// using LocalLib.Casting;
+// using LocalLib.Types;
+// using LocalLib.Services;
 
-namespace LocalLib.Scripting.Actions
-{
-    public class PlaySoundAction : Action
-    {
-        private AudioService audioService;
-        private string filename;
+// namespace LocalLib.Scripting.Actions
+// {
+//     public class PlaySoundAction : Action
+//     {
+//         private AudioService audioService;
+//         private string filename;
 
-        public PlaySoundAction(AudioService audioService, string filename)
-        {
-            this.audioService = audioService;
-            this.filename = filename;
-        }
+//         public PlaySoundAction(AudioService audioService, string filename)
+//         {
+//             this.audioService = audioService;
+//             this.filename = filename;
+//         }
 
-        public void Execute(Cast cast, Script script, ActionCallback callback)
-        {
-            // System.Console.WriteLine("exicute audio");
-            Sound sound = new Sound(filename);
-            audioService.PlaySound(sound);
-            script.RemoveAction(Constants.OUTPUT, this);
-        }
-    }
-}
+//         public void Execute(Cast cast, Script script, ActionCallback callback)
+//         {
+//             // System.Console.WriteLine("exicute audio");
+//             Sound sound = new Sound(filename);
+//             audioService.PlaySound(sound);
+//             script.RemoveAction(Constants.OUTPUT, this);
+//         }
+//     }
+// }
